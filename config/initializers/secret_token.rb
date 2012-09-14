f999a67e50525bad51aa9480a3d44b0fbdd6abcc
Anton Taraev <anti191@gmail.com>
2012-09-14 19:07:18 +0400

    Add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astest::Application.config.secret_token = 'ac8df76267dca84338d6b5aa804140c667ffacf175bf3004a981e20e11da3b93f1219c9895d6170474bf9ebd7e582a8f9726a2e77b859eb8f0e6c78c83c5e9c2'
